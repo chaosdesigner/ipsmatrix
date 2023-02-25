d7d18e72dff98ed5baab34ae6acef4817a1ca085
Vladimir Stepan <lajos@chaosdesign.net>
2023-02-25 23:34:33 +0100

    Backend forms and styles

--- app/helpers/states_helper.rb
+++ app/helpers/states_helper.rb
@@ -6,10 +6,14 @@ module StatesHelper
     if (state.empty?)
       checked = ' '
       form_url = "/states/new?character=#{character_id}&entity=#{entity_id}&sex=#{sex_id}"
+      numeric_values = ''
     else
       checked = ' checked '
       form_url = "/states/#{state.first.id}/edit"
+      if (state.first.character.feature_type_id === 1)
+        numeric_values = "[#{state.first.value_min}, #{state.first.value_max}]"
+      end
     end
-    "<input#{checked}type=\"checkbox\" class=\"key_checkbox\" onClick=\"window.location.assign('#{form_url}');\">".html_safe
+    "<input#{checked}type=\"checkbox\" class=\"key_checkbox\" onClick=\"window.location.assign('#{form_url}');\"><br />".html_safe + numeric_values
   end
 end
