07121890f4f60984d168bd5bc62aac41fcc87161
Vladimir Stepan <lajos@chaosdesign.net>
2023-03-01 22:21:10 +0100

    Added session management

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,45 @@
 class PagesController < ApplicationController
+  before_action :preload, :only => [:home, :output]
 
   def home
-    @entities = Entity.all.order('seq')
     @features = Feature.all.order('seq')
+    session[:states] = []
+    session[:characters] = {}
+  end
+
+  def output
+
+    # Process params and store session
+
+    params.each do |param|
+      if param[0].include? 'checkbox'
+        state_id = param[0].split('_').second
+        if param[1].to_i === 1
+          if session[:states].empty?
+            session[:states] = [state_id]
+          elsif !(session[:states].include? state_id)
+            session[:states] = session[:states] << state_id
+          end
+        elsif param[1].to_i === 0
+          if !session[:states].empty? && (session[:states].include? state_id)
+            session[:states].delete(state_id)
+          end
+        end
+      elsif param[0].include? 'character'
+        character_id = param[0].split('_').second
+        session[:characters][character_id] = param[1]
+      end
+    end
+
+
+
+    render partial: 'output'
+  end
+
+  private
+
+  def preload
+    @entities = Entity.all.order('seq')
     @states = State.all
     @sexes = Sex.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'pages#home'
+
+  get  'pages/output' => 'pages#output'
 end
