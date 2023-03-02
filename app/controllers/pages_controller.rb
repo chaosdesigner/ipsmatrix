7c1039836a2afa97b083cfd6eacda80d3c7a4d4b
Vladimir Stepan <lajos@chaosdesign.net>
2023-03-02 02:55:44 +0100

    Fix key logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -32,15 +32,12 @@ class PagesController < ApplicationController
     end
 
     # Build SQL query
-
-    sql_characters = []
-    entities = []
-    sql = nil
+    
+    sql_selects = entities = []
     if (!session[:characters].empty? || !session[:states].empty?)
-      sql = 'SELECT entity_id, sex_id FROM states WHERE '
       if !session[:characters].empty?
         session[:characters].each do |character|
-          sql_characters << "(value_min <= #{character[1]} AND value_max >= #{character[1]} AND character_id = #{character[0]})"
+          sql_selects << "SELECT entity_id, sex_id FROM states WHERE value_min <= #{character[1]} AND value_max >= #{character[1]} AND character_id = #{character[0]}"
         end
       end
       if !session[:states].empty?
@@ -48,18 +45,16 @@ class PagesController < ApplicationController
         session[:states].each do |state|
           characters << State.find(state).character_id
         end
-        sql_states = "character_id IN (#{characters.join(',')})"
-      end
-
-      if sql_states
-        sql = "#{sql}#{sql_states}"
-        if !sql_characters.empty?
-          characters = sql_characters.join(' OR ')
-          sql = "#{sql} OR #{characters}"
+        features = {}
+        characters.each do |character|
+          feature_id = Character.find(character).feature_id
+          features[feature_id] = features[feature_id] ? features[feature_id] << character : [character]
         end
-      elsif !sql_characters.empty?
-        sql = "#{sql}#{sql_characters.join(' OR ')}"
+        features.each do |feature|
+          sql_selects << "SELECT entity_id, sex_id FROM states WHERE character_id IN (#{feature.join(',')})"
+        end
       end
+      sql = sql_selects.join(' INTERSECT ')
 
       selected_entities = State.find_by_sql(sql)
       selected_entities.each do |entity|
@@ -67,9 +62,11 @@ class PagesController < ApplicationController
       end
 
       session[:query] = entities
+      session[:sql] = sql
     else
       session[:query] = []
-    end
+      session[:sql] = []
+    end      
 
     render partial: 'output'
   end
