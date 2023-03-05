82d2b5e3363ba6bdad34917e9c386ba88958a1f3
Vladimir Stepan <lajos@chaosdesign.net>
2023-03-05 20:26:41 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Ipsmatrix
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "chaosdesign.net"
   end
 end
