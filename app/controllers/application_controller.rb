50fc40f5d28cdcbefaf5287e15a57e3d138927b3
Vladimir Stepan <lajos@chaosdesign.net>
2023-03-11 18:05:55 +0100

    Add galleries for states with multiple images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
     @language = 'cz'
     @name_attribute = 'name_' + @language
     @desc_attribute = 'description_' + @language
+    @host_feature_id = 10
   end
 end
